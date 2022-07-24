353cfe1f14296a7747462dbab0c614e0e636a7ec
卞旭 <254804495@qq.com>
2022-07-24 23:25:41 +0800

    测试push

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,27 +1,21 @@
 // import { SearchOutlined } from 'antd/lib/icon';
-import React, { useState, useEffect } from 'react'
-import { connect } from 'dva'
+import React, { useState, useEffect } from "react";
+import { connect } from "dva";
 
-import { Board } from './../../component'
-import './index.less'
+import { Board } from "./../../component";
+import "./index.less";
 
-const Home = ({
-  
-}) => {
-   
-    useEffect(() => {
-       
-    }, [])
-   
+const Home = ({}) => {
+  useEffect(() => {}, []);
+  console.log("测试push");
 
-    return (
-        <div className={'homePageContain'}>
-            <div className='boardContain0'> <Board></Board></div>
-           
-        </div>
-    );
+  return (
+    <div className={"homePageContain"}>
+      <div className="boardContain0">
+        <Board></Board>
+      </div>
+    </div>
+  );
 };
 // export default Home
-export default connect(({ app }) => ({
-   
-}))(Home)
+export default connect(({ app }) => ({}))(Home);
